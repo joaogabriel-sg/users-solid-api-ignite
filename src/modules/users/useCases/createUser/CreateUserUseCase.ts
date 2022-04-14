9893d98d1987e298d844e77b94b9a54dac8de373
João Gabriel <main.joaogabrielsg@gmail.com>
2022-04-13 23:37:15 -0300

    docs: add swagger api documentation

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -13,7 +13,7 @@ class CreateUserUseCase {
     const userAlreadyExists = this.usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
-      throw new Error("User already exists");
+      throw new Error("This email is already in use");
     }
 
     return this.usersRepository.create({ name, email });
